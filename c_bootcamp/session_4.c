6bfdba01402c4017aba3aaa6de3672bcce563023
jjeecky <146842496+jjeecky@users.noreply.github.com>
2023-10-20 13:57:17 +0000

    session4 worksheet

---
+++ c_bootcamp/session_4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/*
+int sum(int a, int b)
+{
+    int answer;
+    answer = a + b;
+    return answer;
+}
+
+int main()
+{
+    int y =2;
+    int z = sum(5, y);
+    printf("The sum of 5 and %d is %d\n, y, z");
+}
+*/
+
+int main()
+{
+    float radius = 5f;
+    float circle;
+    area = 2 * 3.14 * radius
+    printf("The area is: %d\n")
+
+}
